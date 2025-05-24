5f840323aafc07d781a7b553bcab324d7af366aa
SethHolt <josephmarc@sethholt.co.uk>
2025-05-24 13:26:04 +0100

    Completed exercises 6-11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0 || a === b) return "ERROR";
+    
+    sum = 0;
+    if(a > b) {
+        [a, b] = [b, a];
+    }
+
+    for (b; b >= a; b--) {
+        sum +=b;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  celsius = (num - 32) * 5 / 9;
+  return +Number(celsius).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  fahr = num * 9 / 5 + 32;
+  return +Number(fahr).toFixed(1);
 };
 
 // Do not edit below this line
